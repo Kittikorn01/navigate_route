245ac42cb9a486f761a14625bb65f01b7b996fce
kittikorn <kittikornt01@gmail.com>
2025-09-04 16:48:38 +0700

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:navigate_route/screens/detail_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = '/';
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.pushNamed(
+                  context,
+                  DetailScreen.routeName,
+                  arguments: {
+                    // ส่ง arguments เป็น Map
+                    'id': 'ARG-789',
+                    'title': 'ข้อมูลจาก Named Route Args',
+                  },
+                );
+              },
+              child: const Text('Go to Detail Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                Navigator.pushNamed(
+                  context,
+                  DetailScreen.routeName,
+                  arguments: {
+                    // ส่ง arguments เป็น Map
+                    'id': 'ARG-789',
+                    'title': 'ข้อมูลจาก Named Route Args',
+                  },
+                );
+              },
+              child: const Text('Go to Detail Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
